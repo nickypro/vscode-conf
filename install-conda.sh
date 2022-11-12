aa6d29f867df972c6ef8e6815b9f8b07adcf2d15
Nicky Pochinkov <52249105+pesvut@users.noreply.github.com>
2022-11-12 07:45:15 -0800

    Create install-conda.sh

---
+++ install-conda.sh
@@ -0,0 +1,9 @@
+# Install conda
+sudo apt update --yes
+sudo apt upgrade --yes
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+bash ~/miniconda.sh -b -p ~/miniconda 
+rm ~/miniconda.sh
+export PATH=~/miniconda/bin:$PATH
+
+echo "PATH=$HOME/miniconda/bin:$PATH" >> .bashrc
